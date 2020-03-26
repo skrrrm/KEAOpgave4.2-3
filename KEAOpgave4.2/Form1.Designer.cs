f7ee740cab4f5b86e3b251347755dd478312b336
skrrrm <a@macbook-windows>
2020-03-26 13:33:52 +0100

    Add project files.

---
+++ KEAOpgave4.2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KEAOpgave4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textBoxTableNum = new System.Windows.Forms.TextBox();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(72, 152);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(269, 554);
+            this.listBoxResult.TabIndex = 0;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(67, 36);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(634, 25);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Write a number for the table you want to see and click the button:";
+            // 
+            // textBoxTableNum
+            // 
+            this.textBoxTableNum.Location = new System.Drawing.Point(72, 84);
+            this.textBoxTableNum.Name = "textBoxTableNum";
+            this.textBoxTableNum.Size = new System.Drawing.Size(91, 31);
+            this.textBoxTableNum.TabIndex = 2;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(217, 79);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(124, 40);
+            this.buttonConfirm.TabIndex = 3;
+            this.buttonConfirm.Text = "Ok";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(379, 84);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 25);
+            this.labelError.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 813);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.textBoxTableNum);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.listBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textBoxTableNum;
+        private System.Windows.Forms.Button buttonConfirm;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ KEAOpgave4.2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave4._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            listBoxResult.Items.Clear(); // clear the box on button click
+
+            // Variables
+            int f = 2, i = 0, m;
+            string v = "";
+
+            // try - catch for double
+            try
+            {
+                f = Convert.ToInt32(textBoxTableNum.Text);
+            }
+            catch 
+            {
+                labelError.Text = "Numbers only."; // error text
+            }
+
+            while(i < 10) // while i is less than 10
+            {
+                i++; // i = i + 1
+                m = f * i; 
+
+                v = $"f: {f,3}    i: {i,3}    m: {m,3}"; // string to write out
+                listBoxResult.Items.Add(v); // string added to listBox
+            }
+        }
+    }
+}
